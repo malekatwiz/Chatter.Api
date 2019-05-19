4e3655c52a56c457edd43857b5661ae76467c53f
Malek Atwiz <malekatwiz@gmail.com>
2019-05-19 18:00:55 -0400

    Created API project.

--- Chatter.Api/Models/Message/MessageModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Chatter.Api.Models.Message
-{
-    public class MessageModel
-    {
-        public string Text { get; set; }
-        public string By { get; set; }
-    }
-}
--- Chatter.Api/Models/Message/SendModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Chatter.Api.Models.Message
-{
-    public class SendModel
-    {
-        public string Text { get; set; }
-        public string By { get; set; }
-    }
-}
